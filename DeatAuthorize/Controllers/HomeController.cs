bb7eba12ae52d26930f5dedd1ee5d6ed6a74468d
Dinh Cong Tan <f7deat@users.noreply.github.com>
2018-02-15 08:53:20 +0700

    final

--- DeatAuthorize/Controllers/HomeController.cs
+++ DeatAuthorize/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DeatAuthorize.Common;
+using DeatAuthorize.Models;
+using DeatAuthorize.Security;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,18 +15,72 @@ namespace DeatAuthorize.Controllers
         {
             return View();
         }
-
+        [DeatAuthorize(Order = 3)]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
-
+        [DeatAuthorize(Order = 2)]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginModel login)
+        {
+            List<AccountModel> ListAccount = new List<AccountModel>()
+            {
+            new AccountModel()
+            {
+                Username = "Member",
+                Password = "123",
+                Role = 1
+            },
+            new AccountModel()
+            {
+                Username = "Moderator",
+                Password = "123",
+                Role = 2
+            },
+            new AccountModel()
+            {
+                Username = "Admin",
+                Password = "123",
+                Role = 3
+            }
+            };
+            var res = ListAccount.Count(x => x.Username == login.Username && x.Password == login.Password);
+
+            if (res > 0)
+            {
+                var user = ListAccount.FirstOrDefault(x => x.Username == login.Username);
+                var userSession = new User();
+                userSession.Username = user.Username;
+                userSession.Role = user.Role;
+                Session.Add(CommonConstants.USER_SESSION, userSession);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Sai tên đăng nhập hoặc mật khẩu");
+            }
+            return View();
+        }
+        public ActionResult Loguot()
+        {
+            Session[CommonConstants.USER_SESSION] = null;
+            return Redirect("/Home/Login");
+        }
+        public ActionResult NotAuthorize()
+        {
             return View();
         }
     }
---
+++ DeatAuthorize/Security/DeatAuthorizeAttribute.cs
@@ -0,0 +1,38 @@
+﻿using DeatAuthorize.Common;
+using DeatAuthorize.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace DeatAuthorize.Security
+{
+    public class DeatAuthorizeAttribute: AuthorizeAttribute
+    {
+        //1: Member
+        //2: Moderator
+        //3: Admin
+        //4: ...
+
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+
+            AccountModel db = new AccountModel();
+            var session = (User)HttpContext.Current.Session[CommonConstants.USER_SESSION];
+            if (session == null)
+            {
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "Login", action = "Index", area = "Admin" }));
+            }
+            else
+            {
+                var hasRole = session.Role;
+                if (hasRole < Order)
+                {
+                    filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "Home", action = "NotAuthorize" }));
+                }
+            }
+        }
+    }
+}
